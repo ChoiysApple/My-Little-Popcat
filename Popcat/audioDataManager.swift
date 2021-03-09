640fd56ae7d08cc9f774173c267c819cba194d05
ChoiysApple <chcs1370@gmail.com>
2021-03-09 15:16:45 +0900

    Response to Landscape orientation

---
+++ Popcat/audioDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  audioDataManager.swift
+//  Popcat
+//
+//  Created by Daegeon Choi on 2021/03/09.
+//
+
+import Foundation
---
+++ Popcat/imageResourceManager.swift
@@ -0,0 +1,8 @@
+//
+//  imageResourceManager.swift
+//  Popcat
+//
+//  Created by Daegeon Choi on 2021/03/09.
+//
+
+import Foundation
