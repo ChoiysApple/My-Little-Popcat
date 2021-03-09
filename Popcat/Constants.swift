fcb6d84ee2e8b871b05dcbe0faed77877a0061a9
ChoiysApple <chcs1370@gmail.com>
2021-03-09 13:16:32 +0900

    Create Constants.swift

---
+++ Popcat/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Popcat
+//
+//  Created by Daegeon Choi on 2021/03/09.
+//
+
+import Foundation
+
+struct audioFileName {
+    
+    static let popOriginal = "popcat_original_sound"
+    static let muyaho = "muyaho"
+}
