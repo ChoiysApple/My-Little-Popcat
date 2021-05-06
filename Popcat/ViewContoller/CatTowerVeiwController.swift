2fde043187ae73069196efb6f45db9aa9b1cbf6d
ChoiysApple <chcs1370@gmail.com>
2021-05-06 14:09:33 +0900

    Center align collectionview cell

--- Popcat/ViewContoller/CatTowerVeiwController.swift
+++ Popcat/ViewContoller/CatTowerVeiwController.swift
@@ -19,6 +19,7 @@ class CatTowerVeiwController: UIViewController {
         popCountSwitch.setOn(switchState, animated: false)
         
         collectionView.register(UINib(nibName: "CatTowerCell", bundle: nil), forCellWithReuseIdentifier: "cell")
+        setupFlowLayout(numberOfCells: 2.0)
     }
 
     
@@ -32,7 +33,7 @@ class CatTowerVeiwController: UIViewController {
     }
 }
 
-extension CatTowerVeiwController: UICollectionViewDataSource, UICollectionViewDelegate {
+extension CatTowerVeiwController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 3
@@ -41,11 +42,36 @@ extension CatTowerVeiwController: UICollectionViewDataSource, UICollectionViewDe
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CatTowerCell
         
-//        cell.cellImage.image = #imageLiteral(resourceName: "popcat_closed")
+        cell.cellImage.image = #imageLiteral(resourceName: "popcat_closed")
         cell.cellName.text = "Popcat"
         
         return cell
     }
     
-    
+
 }
+
+extension CatTowerVeiwController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let leftAndRightPaddings: CGFloat = 45.0
+        let numberOfItemsPerRow: CGFloat = 2.0
+    
+        let width = (collectionView.frame.width-leftAndRightPaddings)/numberOfItemsPerRow
+        return CGSize(width: width, height: width)
+    }
+    
+    
+    private func setupFlowLayout(numberOfCells: CGFloat) {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+
+        let halfWidth = UIScreen.main.bounds.width / numberOfCells
+        flowLayout.itemSize = CGSize(width: halfWidth * 0.8 , height: halfWidth * 0.8)
+        flowLayout.minimumInteritemSpacing = 0
+        flowLayout.minimumLineSpacing = halfWidth * 0.1
+        
+        self.collectionView.collectionViewLayout = flowLayout
+    }
+}
+
