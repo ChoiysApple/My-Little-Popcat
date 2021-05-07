e9207f0229f8f1eefe69e2742083a3fd70a2b746
ChoiysApple <chcs1370@gmail.com>
2021-05-07 15:42:33 +0900

    Change collectionview cell image size

--- Popcat/CatTowerCell.swift
+++ Popcat/CatTowerCell.swift
@@ -11,6 +11,7 @@ class CatTowerCell: UICollectionViewCell {
 
     @IBOutlet weak var cellImage: UIImageView!
     @IBOutlet weak var cellName: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
