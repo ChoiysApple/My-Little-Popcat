ff0d0c2cdf1bd19c4ff03224c407093fa3e3a723
ChoiysApple <chcs1370@gmail.com>
2021-05-26 13:07:37 +0900

    Change UserDataManager to handle custom struct

--- Popcat/UserDataManager.swift
+++ Popcat/UserDataManager.swift
@@ -9,52 +9,54 @@ import Foundation
 
 class UserDataManager {
     
+    private let userDefaults = UserDefaults.standard
+    
     //MARK:- Setters
     func setCatData(catData: AssetData?) {
+        do {
+            try userDefaults.setObject(catData, forKey: UserDataKey.currentCatData)
+        } catch {
+            print(error.localizedDescription)
+        }
         
-        let newData = catData ?? defaultAssetData
-        UserDefaults.standard.set(newData.catName, forKey: UserDataKey.currentCatName)
-        UserDefaults.standard.set(newData.openedImageName, forKey: UserDataKey.touchDownImage)
-        UserDefaults.standard.set(newData.closedImageName, forKey: UserDataKey.touchUpImage)
-        UserDefaults.standard.set(newData.mainImageName, forKey: UserDataKey.mainImage)
-        UserDefaults.standard.set(newData.audioSourceName, forKey: UserDataKey.popSound)
     }
     
     func setPopCount(popCount: Int) {
-        UserDefaults.standard.set(popCount, forKey: UserDataKey.popCount)
+        userDefaults.set(popCount, forKey: UserDataKey.popCount)
     }
     
     func setPopVisibility(popVisibility: Bool) {
-        UserDefaults.standard.set(popVisibility, forKey: UserDataKey.popCountVisibility)
+        userDefaults.set(popVisibility, forKey: UserDataKey.popCountVisibility)
     }
 
     func setIsInitialLaunch(isFirst: Bool) {
-        UserDefaults.standard.set(isFirst, forKey: UserDataKey.isNotFirstLaunch)
+        userDefaults.set(isFirst, forKey: UserDataKey.isNotFirstLaunch)
     }
     
     
     //MARK:- Getters
     func getCatData() -> AssetData {
         
-        let newCatName = UserDefaults.standard.string(forKey: UserDataKey.currentCatName) ?? defaultAssetData.catName
-        let newClosedImageName = UserDefaults.standard.string(forKey: UserDataKey.touchUpImage) ?? defaultAssetData.closedImageName
-        let newOpenedImageName = UserDefaults.standard.string(forKey: UserDataKey.touchDownImage) ?? defaultAssetData.openedImageName
-        let newMainImageName = UserDefaults.standard.string(forKey: UserDataKey.mainImage) ?? defaultAssetData.mainImageName
-        let newAudioSourceName = UserDefaults.standard.string(forKey: UserDataKey.popSound) ?? defaultAssetData.audioSourceName
+        var catData = defaultAssetData
+        do {
+            catData = try userDefaults.getObject(forKey: UserDataKey.currentCatData, castTo: AssetData.self)
+        } catch {
+            print(error.localizedDescription)
+        }
         
-        return AssetData(catName: newCatName, closedImageName: newClosedImageName, openedImageName: newOpenedImageName, mainImageName: newMainImageName, audioSourceName: newAudioSourceName)
+        return catData
     }
     
     func getPopCount() -> Int {
-        return UserDefaults.standard.integer(forKey: UserDataKey.popCount)
+        return userDefaults.integer(forKey: UserDataKey.popCount)
     }
     
     func getPopVisibility() -> Bool {
-        return UserDefaults.standard.bool(forKey: UserDataKey.popCountVisibility)
+        return userDefaults.bool(forKey: UserDataKey.popCountVisibility)
     }
     
     func getIsInitialLaunch() -> Bool {
-        return UserDefaults.standard.bool(forKey: UserDataKey.isNotFirstLaunch)
+        return userDefaults.bool(forKey: UserDataKey.isNotFirstLaunch)
     }
     
 }
