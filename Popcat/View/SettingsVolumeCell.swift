62bf383fd04e749f1d510b506011454855770c06
ChoiysApple <chcs1370@gmail.com>
2021-05-31 14:40:42 +0900

    Create SettingVolumeCell

---
+++ Popcat/View/SettingsVolumeCell.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsVolumeCell.swift
+//  Popcat
+//
+//  Created by Daegeon Choi on 2021/05/28.
+//
+
+import UIKit
+
+class SettingsVolumeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
