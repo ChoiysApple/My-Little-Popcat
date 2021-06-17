f703aca72f9ca44ad2219758b1bea342960e8444
ChoiysApple <chcs1370@gmail.com>
2021-06-17 16:09:41 +0900

    Change DeveloperInfo Tableview UI

--- Popcat/Contoller/DeveloperInfoViewController.swift
+++ Popcat/Contoller/DeveloperInfoViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 class DeveloperInfoViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        tableView.isScrollEnabled = false
         // Do any additional setup after loading the view.
     }
     
@@ -21,12 +22,16 @@ class DeveloperInfoViewController: UIViewController {
 
 extension DeveloperInfoViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return DeveloperInfoSection.allCases.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: Identifier.DeveloperPageCell)
-        guard let section = DeveloperInfoSection(rawValue: indexPath.row) else { return UITableViewCell() }
+        guard let section = DeveloperInfoSection(rawValue: indexPath.section) else { return UITableViewCell() }
         
         cell.textLabel?.text = section.title
         cell.detailTextLabel?.text = section.subtitle
@@ -46,12 +51,25 @@ extension DeveloperInfoViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        guard let section = DeveloperInfoSection(rawValue: indexPath.row) else { return }
+        guard let section = DeveloperInfoSection(rawValue: indexPath.section) else { return }
         
         if let url = URL(string: section.pageURL) {
             UIApplication.shared.open(url)
         }
         
         tableView.cellForRow(at: indexPath)?.isSelected = false
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0.0
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        if section != DeveloperInfoSection.allCases.count - 1 {
+            return 10
+        }else{
+            return 0
+        }
+
     }
 }
