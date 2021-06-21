9cc89dd2bba84e1fedf7393a0f75653fd7f376dc
ChoiysApple <chcs1370@gmail.com>
2021-06-21 16:50:37 +0900

    Create initCell() in CatTowerCell

--- Popcat/View/CatTowerCell.swift
+++ Popcat/View/CatTowerCell.swift
@@ -15,6 +15,26 @@ class CatTowerCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+
     }
+    
+    func initCell(catData: AssetData, currentCatName: String) {
+        self.cellImage.image = UIImage(named: catData.mainImageName)
+        self.cellName.text = catData.catName
+        
+        // Make border of CollectionView Cell for selected Cell
+        if currentCatName == catData.catName{
+            self.cellView.layer.borderWidth = 1.5
+        } else {
+            self.cellView.layer.borderWidth = 0.5
+        }
+        
+        self.cellView.layer.borderColor = UIColor(named: "Color")?.cgColor
+
+        //        cell.cellView.layer.borderWidth = 0.0
+        //        cell.cellView.backgroundColor = .systemGray6
+        //        cell.cellName.text = "899/1000"
+    }
+    
+    
 }
