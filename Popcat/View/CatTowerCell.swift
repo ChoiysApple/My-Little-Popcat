91e7188f4735e413984c2395d3e298fcb08308d6
ChoiysApple <chcs1370@gmail.com>
2021-07-11 19:17:07 +0900

    Fix rough cat tower cell border for 4 inch display

--- Popcat/View/CatTowerCell.swift
+++ Popcat/View/CatTowerCell.swift
@@ -35,9 +35,9 @@ class CatTowerCell: UICollectionViewCell {
         
         // Make border of CollectionView Cell for selected Cell
         if currentCatName == catData.catName{
-            self.cellView.layer.borderWidth = 1.5
+            self.cellView.layer.borderWidth = 2.0
         } else {
-            self.cellView.layer.borderWidth = 0.5
+            self.cellView.layer.borderWidth = 0.75
         }
         
         self.cellView.layer.borderColor = UIColor(named: "Color")?.cgColor
