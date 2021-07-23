0c63d08252230bbe49d8c653d6be7401cd293633
ChoiysApple <chcs1370@gmail.com>
2021-07-23 14:32:20 +0900

    Change selected cell borderWidth

--- Popcat/View/CatTowerCell.swift
+++ Popcat/View/CatTowerCell.swift
@@ -35,7 +35,7 @@ class CatTowerCell: UICollectionViewCell {
         
         // Make border of CollectionView Cell for selected Cell
         if currentCatName == catData.catName.localized {
-            self.cellView.layer.borderWidth = 2.0
+            self.cellView.layer.borderWidth = 2.5
         } else {
             self.cellView.layer.borderWidth = 0.75
         }
